796cbf645bbbb3eae747cae4c8ac33bbfe1fa833
jbeli <mohem12ali@gmail.com>
2021-03-04 02:32:29 +0100

    end commit

---
+++ app/src/main/java/com/orange/pokemon/data/PokemonEntyty.kt
@@ -0,0 +1,19 @@
+package com.orange.pokemon.data
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "pokemon_table")
+
+data class PokemonEntyty(
+    @PrimaryKey(autoGenerate = true)
+    var id:Int = 0,
+    val imageurl: String,
+    val name: String,
+    val category: String,
+    val height: String
+
+
+)
---
+++ app/src/main/java/com/orange/pokemon/data/PokimonDao.kt
@@ -0,0 +1,19 @@
+package com.orange.pokemon.data
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query
+
+@Dao
+interface PokimonDao {
+
+
+    @Insert
+    suspend fun InsertONe(obj:PokemonEntyty)
+
+    @Query("SELECT * FROM pokemon_table")
+    suspend fun getALLPokimon() : List<PokemonEntyty>
+
+
+    @Insert
+    suspend fun insertAll(entities: List<PokemonEntyty>)
+}
